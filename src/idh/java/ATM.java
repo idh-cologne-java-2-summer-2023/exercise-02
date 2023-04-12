8a4c18d9d9135d4aa47b2197168252cea82b8ee8
dvowe <david.vowe@gmail.com>
2023-04-12 12:42:44 +0200

    changed src code to my own

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -1,46 +1,50 @@
 package idh.java;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class ATM {
-	int accountBalance = 100;
-
+	
+	int balance;
 	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
-	 * number to the function cashout(...) which actually does the calculation and
-	 * produces money. If the user enters anything else than an integer number, the
-	 * loop breaks and the program exists
+	 * Main command loop of the ATM.
+	 * Asks the user to enter a number, and passes this number to the function cashout(...) 
+	 * which actually does the calculation and produces money.
+	 * If the user enters anything else than an integer number, the loop breaks 
+	 * and the program exits
 	 */
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while (true) {
+		while(true) {
 			try {
 				System.out.print("Enter the amount to withdraw: ");
-				int amount = Integer.parseInt(br.readLine());
+				String input = br.readLine();
+				if (input.toLowerCase().equals("exit")) {
+					System.exit(0);
+				}
+				int amount = Integer.parseInt(input);
 				cashout(amount);
 			} catch (Exception e) {
+				System.out.println("illegal input");
 				break;
 			}
 		}
 	}
-
+	
 	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
+		if (amount <= balance) {
 			System.out.println("Ok, here is your money, enjoy!");
-		} else {
-			System.out.println("Sorry, not enough money in the bank.");
+			balance -= amount;
 		}
-
-	};
-
+		else {
+			System.out.println("Sorry, not enough money in the bank");
+		}
+	}
+	
 	/**
 	 * Launches the ATM
 	 */
 	public static void main(String[] args) {
 		ATM atm = new ATM();
 		atm.run();
-	};
-
+	}
 }
