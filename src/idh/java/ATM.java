901200881180904eef49e7461632b622c575ee1e
annaa <annaa@LAPTOP-18NIQUH0.speedport.ip>
2023-04-15 22:01:15 +0200

    Commit 1: edited the cashout method

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -4,14 +4,16 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class ATM {
+	
 	int accountBalance = 100;
 
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
 	 * number to the function cashout(...) which actually does the calculation and
 	 * produces money. If the user enters anything else than an integer number, the
-	 * loop breaks and the program exists
+	 * loop breaks and the program exits
 	 */
+	
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
@@ -26,13 +28,14 @@ public class ATM {
 	}
 
 	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
-			System.out.println("Ok, here is your money, enjoy!");
+		if (amount <= accountBalance) {
+			accountBalance -= amount;
+			System.out.println("Here is your money, enjoy! Current amount in the bank: " + accountBalance);
+		} else if (amount == 0) {
+			System.out.println("Error: cannot withdraw the amount 0 from bank account. Please enter a new amount.");
 		} else {
-			System.out.println("Sorry, not enough money in the bank.");
+			System.out.println("Sorry, not enough money in the bank. Current amount in the bank: " + accountBalance);
 		}
-
 	};
 
 	/**
