46fd64b57783d2de19124c0e47872d5a3575fe7e
erhan <erhan.guerleyik@gmail.com>
2023-04-16 12:40:44 +0200

    Eigene Lösung für die Aufgabe eingefügt.

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -3,18 +3,22 @@ package idh.java;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class ATM {
-	int accountBalance = 100;
+import b.ATM;
 
+public class ATM {
+	
+	int cash = 500;
+	
 	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
-	 * number to the function cashout(...) which actually does the calculation and
-	 * produces money. If the user enters anything else than an integer number, the
-	 * loop breaks and the program exists
+	 * Main command loop of the ATM
+	 * Asks the user to enter a number, and passes this number to the function cashout(...) 
+	 * which actually does the calculation and produces money.
+	 * If the user enters anything else than an integer number, the loop breaks 
+	 * and the program exists
 	 */
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while (true) {
+		while(true) {
 			try {
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
@@ -24,23 +28,27 @@ public class ATM {
 			}
 		}
 	}
-
+	
 	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
-			System.out.println("Ok, here is your money, enjoy!");
-		} else {
-			System.out.println("Sorry, not enough money in the bank.");
+		
+		if (cash > amount) {
+			cash -= amount;
+			System.out.println("User specified amount is withdrawn. Enjoy. " + amount + "$.");
+			System.out.println("Your current cash amount is " + cash + "$.");
+		} else if (cash < amount) {
+			System.out.println("Sorry, you are too poor.");
+		} else if (cash == amount) {
+			cash -= amount;
+			System.out.println("User specified amount is withdrawn. Enjoy. " + amount + "$.");
+			System.out.println("Your current cash amount is " + cash + "$.");
 		}
-
-	};
-
+	}
+	
 	/**
 	 * Launches the ATM
 	 */
 	public static void main(String[] args) {
 		ATM atm = new ATM();
 		atm.run();
-	};
-
+	}
 }
