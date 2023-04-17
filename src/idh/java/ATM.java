7ff7a7a21d59508ac7f3b38f2b6540e678ed9a94
DELTA-Dx24 <DELTA-Dx24@archlinux>
2023-04-17 15:55:27 +0200

    Ha 17.4

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -2,38 +2,64 @@ package idh.java;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 
 public class ATM {
-	int accountBalance = 100;
-
-	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
-	 * number to the function cashout(...) which actually does the calculation and
-	 * produces money. If the user enters anything else than an integer number, the
-	 * loop breaks and the program exists
-	 */
+	int atmblc = 230;
+	int[] arr = {2000, 100, 2432, 323423, 1};
+	
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
+				System.out.print("Enter the account number 1-5: ");
+				int useracc = Integer.parseInt(br.readLine());
+				//check if account number is valid return balance for set account
+				int idamount = usernmbr(useracc);
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				//check if account balance and atm cash balance is larger then withdraw amount
+				int blcaftercashout = cashout(amount, idamount);
+				//update array with new balance
+				Array.set(arr, useracc-1, blcaftercashout);
+			    System.out.println("new account balance: " + blcaftercashout + "\ncash left in atm: " + atmblc);
 			} catch (Exception e) {
 				break;
 			}
 		}
 	}
 
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
+	public int cashout(int amount, int idamount) {
+		if (amount < idamount & amount < atmblc) {
+		 	int aftdeductioin = idamount - amount;
+		    atmblc = atmblc - amount;
 			System.out.println("Ok, here is your money, enjoy!");
+			return aftdeductioin;
 		} else {
+			if (amount<idamount) {
+				System.out.println("no cash in atm try other time");
+				return idamount;
+			}
+			else {
 			System.out.println("Sorry, not enough money in the bank.");
+			return idamount;
+			}
 		}
-
-	};
+	}
+	public int usernmbr(int usracc) {
+		try {
+			int wamount = arr[usracc - 1];
+	        Array.set(arr, usracc - 1, wamount);
+			return wamount;
+		}catch(Exception e) {
+			System.out.println("this account number deos not exist");
+			run();
+		}
+		return 0;
+		
+	}
+	
+	;
 
 	/**
 	 * Launches the ATM
