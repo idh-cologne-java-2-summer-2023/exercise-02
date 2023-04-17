c2ab8aead34fada63f780801af9f08eb82895258
skr4ll <skrall_pa@protonmail.com>
2023-04-17 21:43:53 +0200

    Komplett.

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -3,7 +3,7 @@ package idh.java;
 import java.io.*;
 
 public class ATM {
-	float credits = 200f;
+	float credits = 200000f;		//Im ATM vorhandenes Geld
 	
 	/**
 	 * Main command loop of the ATM
@@ -15,8 +15,20 @@ public class ATM {
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		
+		Accounts accs[] = new Accounts[3];
+		 accs[0] = new Accounts(123);
+		 accs[1] = new Accounts(234);
+		 accs[2] = new Accounts(345);
+		
 		while(true) {
 			try {
+				System.out.println("Vorhandenes Bargeld: " + credits);
+				System.out.print("Enter your account number: ");
+				String line0 = br.readLine();
+				int accNum = Integer.parseInt(line0);
+				
+				 				
 				System.out.print("Enter the amount to withdraw: ");
 				String line = br.readLine();
 					if (line.equals("exit")) {
@@ -25,7 +37,7 @@ public class ATM {
 					}
 					else if (line.matches("[+-]?([0-9]*[.])?[0-9]+" )) {
 						float amount = Float.parseFloat(line);
-						cashout(amount);
+						cashout(amount, accNum, accs);
 				    }
 					else {
 						System.out.println("Bitte numerischen Betrag oder exit eingeben");
@@ -36,16 +48,25 @@ public class ATM {
 		}
 	}
 	
-	public void cashout(float amount) {
-		if (credits >= amount && amount > 0) {
-			credits = credits - amount;
-			System.out.println("Ausgabe von: " + amount + " credits. \nNeuer Kontostand: " + credits + " credits");
-		} 
-		else {
-			System.out.println("Unzureichende Credits/Eingabe eines Negativen Wertes");	
-	    }
-			
-	}
+	public void cashout(float amount, int accNum, Accounts[] accs) {
+			if (credits < amount) {
+				System.out.println("Sorry, the ATM doesn't have that much cash anymore.");
+			}
+			else {
+				for (int i = 0; i < accs.length; i++) {
+					if (accs[i].accNum == accNum && accs[i].guthaben >= amount && credits >= amount) {
+						accs[i].guthaben = accs[i].guthaben - amount;
+						credits = credits - amount;
+						System.out.println("Ok, here you go!");
+						System.out.println("Restliches Guthaben von Konto: " + accs[i].accNum + " ist: " + accs[i].guthaben );
+					}
+					else if (accs[i].accNum == accNum && accs[i].guthaben < amount){
+						System.out.println("Sorry, you don't have enough money in the bank.");
+						System.out.println("Restliches Guthaben für Konto: " + accs[i].accNum + " ist: " + accs[i].guthaben );
+					}
+				}      
+			}
+	} 
 	
 	/**
 	 * Launches the ATM
