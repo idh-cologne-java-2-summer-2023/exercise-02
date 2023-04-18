a9fc767edac32d4569d185b90323fad04ba86f89
Elice <Elice@BOOK-4N151U59UN.fritz.box>
2023-04-18 21:50:00 +0200

    Hausaufgabe

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class ATM {
-	int accountBalance = 100;
+	int accountBalance ;
+	int fullamountonbank= 1000;
+	BankCustomers customers= new BankCustomers();
 
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
@@ -14,25 +16,56 @@ public class ATM {
 	 */
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	
+		/* new Integer(Integer.parseInt(s, radix))
+		s - the string to be parsed.
+		radix - the radix to be used in interpreting s
+		an Integer object holding the value represented by the string argument in the specified radix.
+		NumberFormatException - if the String does not contain a parsable int.
+		*/
+		
 		while (true) {
 			try {
+				System.out.println("Enter your account number:");
+				int accountnumberinput= Integer.parseInt(br.readLine());
+				customers.accounts( accountnumberinput);
+				customers.code();
+				
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
 				cashout(amount);
 			} catch (Exception e) {
 				break;
+				
 			}
 		}
 	}
+	
+	
+	
 
 	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
+		
+		
+ accountBalance= customers.cashonbank;
+ 
+	
+			
+		if (amount <= accountBalance) {
+			
 			System.out.println("Ok, here is your money, enjoy!");
-		} else {
-			System.out.println("Sorry, not enough money in the bank.");
+			accountBalance= accountBalance-amount;
 		}
-
+		else if (amount>fullamountonbank) {
+			System.out.println("Sorry, there is not enough Money in the Bank.");
+		}
+		
+		else if (amount>accountBalance){
+			System.out.println("Sorry,you don´t have enough money on your account.");
+		}
+	
+		
 	};
 
 	/**
@@ -41,6 +74,13 @@ public class ATM {
 	public static void main(String[] args) {
 		ATM atm = new ATM();
 		atm.run();
+		
+		
+		
+		
+		
+		
+		
 	};
 
 }
---
+++ src/idh/java/BankCustomers.java
@@ -0,0 +1,47 @@
+package idh.java;
+
+public class BankCustomers {
+	
+	int accountnumer;
+	int cashonbank;
+	
+	
+
+	public void accounts(int accountnumber) {
+		
+		this.accountnumer= accountnumber;
+		
+		
+		
+		}
+	public void code () {
+		if(accountnumer==123) {
+			
+			this.cashonbank=500;
+			
+		}
+		if(accountnumer==124) {
+			
+			this.cashonbank=300;
+		}
+		if(accountnumer==125) {
+			
+			this.cashonbank=100;
+		}
+		
+		
+		
+		
+		
+	}
+}
+
+
+	
+	
+	
+	
+
+
+	
+	
