c1ba639eb24098365c678bcb6fc59cd0c06e8337
henni <henni@DESKTOP-1U1DQFP>
2023-04-18 15:30:13 +0200

    excerise-02 (idh-cologne-java-2-summer-2023)

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -1,10 +1,17 @@
 package idh.java;
 
 import java.io.BufferedReader;
+import java.util.Scanner;
+
+
 import java.io.InputStreamReader;
 
 public class ATM {
+	
+	
+	int atmBalance = 50;
 	int accountBalance = 100;
+	int accountNumber;
 
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
@@ -16,24 +23,47 @@ public class ATM {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
+				
+				System.out.print("Enter your account number: ");
+				int accountNumber = Integer.parseInt(br.readLine());
+				access(accountNumber); 
+				
+				
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				cashout(amount); 
+				
+				
+				
 			} catch (Exception e) {
 				break;
 			}
 		}
 	}
 
+//method for validating an account of a user.
+	public void access(int accountNumber) {
+		if (accountNumber >= 3) {
+			System.out.println("Welcome, customer.");
+			}
+}; 
+	
+
 	public void cashout(int amount) {
-		if (amount < accountBalance) {
+		if (amount < accountBalance && amount < atmBalance) {
 			accountBalance = accountBalance - amount;
 			System.out.println("Ok, here is your money, enjoy!");
-		} else {
-			System.out.println("Sorry, not enough money in the bank.");
+		} else if (amount > atmBalance){
+			atmBalance = atmBalance - amount;
+			System.out.println("Sorry, the ATM doesn't have that much cash anymore.");
+		} else if (amount > accountBalance){
+			accountBalance = accountBalance - amount;
+			System.out.println("Sorry, your account doesn't hold that much.");
 		}
 
 	};
+	
+	
 
 	/**
 	 * Launches the ATM
