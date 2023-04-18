e3966988a4cd409a88e56965cdd047d38f0847aa
Leila <Leila@172.29.204.224>
2023-04-18 15:55:20 +0200

    Added support for multiple accounts and ATM cash balance tracking

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -44,3 +44,53 @@ public class ATM {
 	};
 
 }
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class ATM {
+    private int cashInMachine = 500;
+    private HashMap<Integer, Integer> accountBalances = new HashMap<>();
+
+    public ATM() {
+        // Initialize some account balances for testing purposes
+        accountBalances.put(123, 100);
+        accountBalances.put(234, 300);
+        accountBalances.put(345, 50);
+    }
+
+    public void run() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            try {
+                System.out.print("Enter your account number: ");
+                int accountNumber = Integer.parseInt(br.readLine());
+                System.out.print("Enter the amount to withdraw: ");
+                int amount = Integer.parseInt(br.readLine());
+                cashout(accountNumber, amount);
+            } catch (Exception e) {
+                break;
+            }
+        }
+    }
+
+    public void cashout(int accountNumber, int amount) {
+        if (!accountBalances.containsKey(accountNumber)) {
+            System.out.println("Sorry, account not found.");
+        } else if (amount > accountBalances.get(accountNumber)) {
+            System.out.println("Sorry, not enough money in the bank.");
+        } else if (amount > cashInMachine) {
+            System.out.println("Sorry, the ATM doesn't have that much cash anymore.");
+        } else {
+            accountBalances.put(accountNumber, accountBalances.get(accountNumber) - amount);
+            cashInMachine -= amount;
+            System.out.println("Ok, here you go!");
+        }
+    }
+
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+        atm.run();
+    }
+}
