118c2877273ed4354a07bc0f194c71d7ab1a0164
BMusolff <92873072+BMusolff@users.noreply.github.com>
2023-04-19 13:40:10 +0200

    Commit.

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -2,9 +2,21 @@ package idh.java;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
 public class ATM {
-	int accountBalance = 100;
+	
+
+	int atmBalance = 10000;
+
+	// Create a HashMap object called  bankAccount
+    HashMap<Integer, Integer> bankAccount = new HashMap<Integer, Integer>();
+    // Add keys and values (accountNumber, accountBalance)
+    bankAccount.put(123, 25);
+    bankAccount.put(234, 2);
+    bankAccount.put(345, 100);
+
+  
 
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
@@ -16,23 +28,32 @@ public class ATM {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
+				System.out.print("Enter your account number: ");
+                int accountNumber = Integer.parseInt(br.readLine());
+                if (!accountNumber.containsKey(bankAccount)) {
+					System.out.println("Sorry, account not found.");
+					break;
+                }
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				cashout(accountNumber, amount);
 			} catch (Exception e) {
 				break;
 			}
 		}
 	}
 
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
+	public void cashout(int accountNumber, int amount) {
+		if (amount < accountBalance && amount < atmBalance) {
 			accountBalance = accountBalance - amount;
+			atmBalance = atmBalance - amount;
 			System.out.println("Ok, here is your money, enjoy!");
-		} else {
+		} else if (amount > atmBalance) {
+			atmBalance = atmBalance - amount;
+			System.out.println("Sorry, the ATM doesn't have that much cash anymore.");
+ 		} else {
 			System.out.println("Sorry, not enough money in the bank.");
 		}
-
 	};
 
 	/**
@@ -41,6 +62,7 @@ public class ATM {
 	public static void main(String[] args) {
 		ATM atm = new ATM();
 		atm.run();
+		
 	};
 
 }
