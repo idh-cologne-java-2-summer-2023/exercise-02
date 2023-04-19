baa128da53e070a5ddc263c12b9c027a64238dcd
JohannonBlake <johannon.blade@Gmail.com>
2023-04-19 12:01:41 +0200

    I´m working on it, won´t accept a variable

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -16,15 +16,38 @@ public class ATM {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
-				System.out.print("Enter the amount to withdraw: ");
+
+				System.out.println("Enter your account ID:");
+				int accountInput = Integer.parseInt(br.readLine());
+				int accountID1 = 243;
+				int accountID2 = 156;
+				if (accountInput == accountID1){
+					int amount = 200;
+				}
+				else if(accountInput == accountID2){
+					 int amount = 150;
+
+				}
+				else{
+					System.out.println("No account by that number");
+				}
+					
+				
+			 	System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
 				cashout(amount);
+				}
 			} catch (Exception e) {
 				break;
 			}
 		}
-	}
+		}
+	
 
+	
+	
+
+	
 	public void cashout(int amount) {
 		if (amount < accountBalance) {
 			accountBalance = accountBalance - amount;
