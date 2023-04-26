1770b4dbf1e0bb3faf5522e1a1670e10e4c053a1
ischaahmad <ischaahmad@192.168.0.227>
2023-04-26 12:38:02 +0200

    ATM aktualisieren

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -2,16 +2,54 @@ package idh.java;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class ATM {
-	int accountBalance = 100;
-
+	// AccountBalance von jeder ID
+	static int accountBalance1 = 100;
+	static int accountBalance2 = 7100;
+	static int accountBalance3 = 90;
+	
+    //ATM balance 
+	static int ATMbalance = 5000;
+	
+    // Account Id
+	String accountnr1 = "9543";
+	String accountnr2 = "6432";
+	String accountnr3 = "8172";
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
 	 * number to the function cashout(...) which actually does the calculation and
 	 * produces money. If the user enters anything else than an integer number, the
 	 * loop breaks and the program exists
 	 */
+	
+	//Überprüfung der I
+	public void begin() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please enter your account Id: ");
+		
+		 while (true) {
+
+	            String input = sc.next();
+
+	            if (input.equals(accountnr1) || input.equals(accountnr2) || input.equals(accountnr3)) {
+	                ATM atm = new ATM();
+					atm.run();
+	            } else if (input.equals("exit")) {
+	                sc.close();
+	                System.exit(0);
+	            } else {
+	                System.out.println("Access is denied");
+
+	            }
+	        }
+	    }
+
+
+
+		
+	
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
@@ -25,9 +63,27 @@ public class ATM {
 		}
 	}
 
+	// neuer Betrag auf dem ATM und auf den Konten
 	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
+		int totalAccountBalance = accountBalance1 + accountBalance2 + accountBalance3;
+        if (amount <= totalAccountBalance && amount <= ATMbalance) {
+        	
+        	// Das Geld wird von jedem Konto abgezogen
+        	 if (amount <= accountBalance1) {
+                 accountBalance1 = accountBalance1 - amount;
+             } else if (amount <= accountBalance1 + accountBalance2) {
+                 accountBalance2 = accountBalance2 - amount + accountBalance1;
+                 accountBalance1 = 0;
+             } else {
+                 accountBalance3 = accountBalance3 - amount + accountBalance1 + accountBalance2;
+                 accountBalance1 = 0;
+                 accountBalance2 = 0;
+             }
+	
+        }
+		
+     // Automatenbestand aktualisieren
+      if( ATMbalance >= amount) {
 			System.out.println("Ok, here is your money, enjoy!");
 		} else {
 			System.out.println("Sorry, not enough money in the bank.");
@@ -40,7 +96,7 @@ public class ATM {
 	 */
 	public static void main(String[] args) {
 		ATM atm = new ATM();
-		atm.run();
+		atm.begin();
 	};
 
 }
