de3149b6121ef2a31760916a916d20dee484ffa8
Lisa <lisa.krings3@gmail.de>
2023-04-28 13:26:50 +0200

    nochmal hochgeladen

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -4,43 +4,67 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class ATM {
-	int accountBalance = 100;
 
-	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
-	 * number to the function cashout(...) which actually does the calculation and
-	 * produces money. If the user enters anything else than an integer number, the
-	 * loop breaks and the program exists
-	 */
+	int[] accounts = new int[500];
+	int atmbalance = 100;
+	
+	
+	public void accountmanager(){
+		for(int i=0;i<500;i++) {
+			accounts[i] = (int) (Math.random()*(1000));	
+		}	
+	}
+	
+	
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while (true) {
+		accountmanager();
+		while(true) {
 			try {
-				System.out.print("Enter the amount to withdraw: ");
+				System.out.print("Please enter your account number:");
+				int number = Integer.parseInt(br.readLine());
+			if (number<=accounts.length) {
+				System.out.println("Your current balance is " + accounts[number] + "€");
+				System.out.print("Please enter the amount you would like to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				cashout(amount,number);
+				}
+			else {System.out.println("Sorry, we don't have an Account with this number. Please try again.");
+				}
 			} catch (Exception e) {
 				break;
 			}
 		}
 	}
-
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
+	public void cashout(int amount,int number) {
+		int accbalance = accounts[number];
+		if (accbalance >= amount && atmbalance>=amount) {	
 			System.out.println("Ok, here is your money, enjoy!");
-		} else {
-			System.out.println("Sorry, not enough money in the bank.");
+			int newaccbalance = accbalance-amount;
+			accbalance = newaccbalance;
+			int newatmbalance = atmbalance - amount;
+			atmbalance = newatmbalance;
+			System.out.println("Your new balance is "+ accbalance + "€");
+			accounts[number]=newaccbalance;
+			
 		}
-
+		else if(atmbalance<amount){
+			System.out.println("Sorry, the ATM seems to has run out of money. Please contact our Service Hotline.");
+		}
+		else if(accbalance<amount) {
+			System.out.println("Sorry, there is not enough money in your bank account.");
+		}
 	};
-
+	
 	/**
 	 * Launches the ATM
 	 */
+	
 	public static void main(String[] args) {
+		
 		ATM atm = new ATM();
 		atm.run();
 	};
+};
 
-}
+
